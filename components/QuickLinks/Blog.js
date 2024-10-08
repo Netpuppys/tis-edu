7c0a6d1f42fc8971646307457fb45327eea74b0f
Harsh Kumar rattewal <harshrattewal@gmail.com>
2024-10-08 11:48:21 +0530

    Node Js CRm updated

--- components/QuickLinks/Blog.js
+++ components/QuickLinks/Blog.js
@@ -1,112 +1,5 @@
-"use client";
-import React, { useEffect, useState, useCallback } from "react";
-import Image from "next/image";
-import "../../styles/QuickLinks/Blog.css"; // Assuming you have a Blog.module.css file
-import loader from "../../public/loader.svg";
-const Blog = () => {
-  const [blogs, setBlogs] = useState([]);
-  const [page, setPage] = useState(1);
-  const [allBlogsLoaded, setAllBlogsLoaded] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  const fetchblog = useCallback(
-    async (pageno) => {
-      setLoading(true);
-      const response = await fetch(
-        `https://admin.tis.edu.in/wp-json/wp/v2/posts?page=${
-          pageno ? pageno : page
-        }&per_page=18`
-      );
-      const data = await response.json();
-      if (
-        data?.message ===
-        "The page number requested is larger than the number of pages available."
-      ) {
-        setLoading(false);
-        setAllBlogsLoaded(true);
-        return;
-      }
-      setBlogs((prevBlogs) => [...prevBlogs, ...data]);
-      setLoading(false);
-    },
-    [page]
-  );
-
-  useEffect(() => {
-    fetchblog();
-  }, [fetchblog]);
-
-  const handleLoadMore = () => {
-    setPage((prevPage) => prevPage + 1);
-    fetchblog(page + 1);
-  };
-
-  if (loading && blogs.length === 0)
-    return (
-      <>
-        <div className="loader">
-          <Image src={loader} alt="Loading..." />
-        </div>
-      </>
-    );
-  const formatDate = (dateString) => {
-    const options = { year: "numeric", month: "long", day: "numeric" };
-    return new Date(dateString).toLocaleDateString(undefined, options);
-  };
-  return (
-    <>
-      {loading && (
-        <div className="absolute-loader">
-          <Image src={loader} alt="Loading..." />
-        </div>
-      )}
-      <div className="blog-container">
-        <div className="card-grid">
-          {blogs.map((blog) => {
-            const formattedDate = formatDate(blog.date);
-            const formattedTitle = blog.title.rendered
-              .replace(/&#8217;/g, "'")
-              .replace(/&#038;/g, "&");
-            return (
-              <a href={`/${blog.slug}`} key={blog.slug}>
-                <div>
-                  <div className="card-wrapper">
-                    <Image
-                      width="400"
-                      height="250"
-                      src={blog?.yoast_head_json?.og_image?.[0]?.url}
-                      className="cardImage"
-                      alt="Blog Image"
-                    />
-                    <div className="card-text">
-                      <p>{formattedDate}</p>
-                      <h5>{formattedTitle}</h5>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            );
-          })}
-        </div>
-        {allBlogsLoaded ? (
-          <button className="load-more" disabled>
-            All Blogs Loaded
-          </button>
-        ) : (
-          <button className="load-more" onClick={handleLoadMore}>
-            Load More
-          </button>
-        )}
-      </div>
-    </>
-  );
-};
-
-export default Blog;
-
 // "use client";
 // import React, { useEffect, useState } from "react";
-// import Link from "next/link";
 // import Image from "next/image";
 // import loader from "../../public/loader.svg";
 // import "../../styles/QuickLinks/Blog.css";
@@ -121,28 +14,27 @@ export default Blog;
 //     const fetchBlogs = async () => {
 //       try {
 //         const response = await fetch(
-//           `https://blog.repsoft.in/api/v1/post?page=${page}&per_page=18`
+//           `https://admin.tis.edu.in/wp-json/wp/v2/posts?page=${page}&per_page=18`
 //         );
+
+//         // Check if the response status is OK (200-299)
+//         if (!response.ok) {
+//           throw new Error(`HTTP error! Status: ${response.status}`);
+//         }
+
 //         const data = await response.json();
 
-//         if (data.success) {
-//           const filteredBlogs = data.data.filter(
-//             (blog) => blog.category && blog.category.name === "Blogs"
-//           );
-
-//           if (filteredBlogs.length === 0) {
-//             setAllBlogsLoaded(true); // If no blogs are returned, set allBlogsLoaded to true
-//           } else {
-//             setBlogs((prevBlogs) => {
-//               const uniqueBlogs = filteredBlogs.filter(
-//                 (newBlog) =>
-//                   !prevBlogs.some((prevBlog) => prevBlog.slug === newBlog.slug)
-//               );
-//               return [...prevBlogs, ...uniqueBlogs]; // Append only unique blogs
-//             });
-//           }
+//         if (Array.isArray(data) && data.length > 0) {
+//           // Assuming that the posts don't have a specific category structure in the response
+//           setBlogs((prevBlogs) => {
+//             const uniqueBlogs = data.filter(
+//               (newBlog) =>
+//                 !prevBlogs.some((prevBlog) => prevBlog.slug === newBlog.slug)
+//             );
+//             return [...prevBlogs, ...uniqueBlogs]; // Append only unique blogs
+//           });
 //         } else {
-//           console.error("Failed to fetch blogs:", data.message);
+//           setAllBlogsLoaded(true); // If no blogs are returned, set allBlogsLoaded to true
 //         }
 //       } catch (error) {
 //         console.error("Error fetching blogs:", error);
@@ -175,16 +67,18 @@ export default Blog;
 
 //   return (
 //     <>
+//       {loading && (
+//         <div className="absolute-loader">
+//           <Image src={loader} alt="Loading..." />
+//         </div>
+//       )}
 //       <div className="blog-container">
 //         <div className="card-grid">
 //           {blogs.map((blog) => {
-//             const formattedDate = formatDate(blog.created_at);
-
-//             // Check if title and rendered property exist
-//             const formattedTitle = blog.title
-//               ? blog.title.replace(/&#8217;/g, "'").replace(/&#038;/g, "&")
-//               : ""; // Fallback if title is not available
-
+//             const formattedDate = formatDate(blog.date);
+//             const formattedTitle = blog.title.rendered
+//               .replace(/&#8217;/g, "'")
+//               .replace(/&#038;/g, "&");
 //             return (
 //               <a href={`/${blog.slug}`} key={blog.slug}>
 //                 <div>
@@ -192,7 +86,7 @@ export default Blog;
 //                     <Image
 //                       width="400"
 //                       height="250"
-//                       src={blog.banner_img}
+//                       src={blog?.yoast_head_json?.og_image?.[0]?.url}
 //                       className="cardImage"
 //                       alt="Blog Image"
 //                     />
@@ -221,3 +115,121 @@ export default Blog;
 // };
 
 // export default Blog;
+
+"use client";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import loader from "../../public/loader.svg";
+import "../../styles/QuickLinks/Blog.css";
+
+const Blog = () => {
+  const [blogs, setBlogs] = useState([]); // Initialize as an empty array
+  const [page, setPage] = useState(1);
+  const [allBlogsLoaded, setAllBlogsLoaded] = useState(false);
+  const [loading, setLoading] = useState(true); // Set loading to true initially
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const response = await fetch(
+          `https://blog.repsoft.in/api/v1/post?page=${page}&per_page=18`
+        );
+        const data = await response.json();
+
+        if (data.success) {
+          const filteredBlogs = data.data.filter(
+            (blog) => blog.category && blog.category.name === "Blogs"
+          );
+
+          if (filteredBlogs.length === 0) {
+            setAllBlogsLoaded(true); // If no blogs are returned, set allBlogsLoaded to true
+          } else {
+            setBlogs((prevBlogs) => {
+              const uniqueBlogs = filteredBlogs.filter(
+                (newBlog) =>
+                  !prevBlogs.some((prevBlog) => prevBlog.slug === newBlog.slug)
+              );
+              return [...prevBlogs, ...uniqueBlogs]; // Append only unique blogs
+            });
+          }
+        } else {
+          console.error("Failed to fetch blogs:", data.message);
+        }
+      } catch (error) {
+        console.error("Error fetching blogs:", error);
+      } finally {
+        setLoading(false); // Set loading to false regardless of success or failure
+      }
+    };
+
+    fetchBlogs();
+  }, [page]);
+
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
+
+  const handleLoadMore = () => {
+    if (!allBlogsLoaded) {
+      setPage((prevPage) => prevPage + 1); // Increment page to fetch more blogs
+    }
+  };
+
+  if (loading && blogs.length === 0) {
+    return (
+      <div className="loader">
+        <Image src={loader} alt="Loading..." />
+      </div>
+    ); // Display loader if still loading and no blogs are fetched
+  }
+
+  return (
+    <>
+      <div className="blog-container">
+        <div className="card-grid">
+          {blogs.map((blog) => {
+            const formattedDate = formatDate(blog.created_at);
+
+            // Check if title and rendered property exist
+            const formattedTitle = blog.title
+              ? blog.title.replace(/&#8217;/g, "'").replace(/&#038;/g, "&")
+              : ""; // Fallback if title is not available
+
+            return (
+              <a href={`/${blog.slug}`} key={blog.slug}>
+                <div>
+                  <div className="card-wrapper">
+                    <Image
+                      width="400"
+                      height="250"
+                      src={blog.banner_img}
+                      className="cardImage"
+                      alt="Blog Image"
+                    />
+                    <div className="card-text">
+                      <p>{formattedDate}</p>
+                      <h5>{formattedTitle}</h5>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            );
+          })}
+        </div>
+        {allBlogsLoaded ? (
+          <button className="load-more" disabled>
+            All Blogs Loaded
+          </button>
+        ) : (
+          <button className="load-more" onClick={handleLoadMore}>
+            Load More
+          </button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Blog;
