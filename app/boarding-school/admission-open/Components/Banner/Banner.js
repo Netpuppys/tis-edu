1c6df64f163450a2689f50fdc27b4ab785e9d5fe
Harsh Kumar rattewal <harshrattewal@gmail.com>
2024-10-17 13:42:32 +0530

    GoogleReviews

--- app/boarding-school/admission-open/Components/Banner/Banner.js
+++ app/boarding-school/admission-open/Components/Banner/Banner.js
@@ -87,11 +87,11 @@ function Banner() {
     changeImage(currentIndex);
   }, [currentIndex]);
   return (
-    <div className="w-full min-h-[100vh] flex flex-col justify-end md:justify-center items-end relative object-cover bg-[#b90124]">
+    <div className="w-full min-h-[100vh] flex flex-col justify-end md:justify-center items-end relative object-cover bg-transparent md:bg-[#b90124]">
       <div className="absolute top-0 left-0 z-30 px-6 md:px-8 py-6 md:py-8 flex items-center justify-between w-full">
         <a
           href="tel:+91-8108012525"
-          className="w-20 md:w-28 bg-[#60BAB1] p-4 md:p-6 rounded-full"
+          className="w-20 md:w-28 bg-[#60BAB1] p-6 md:p-8 rounded-full"
         >
           <Image src={phoneCallGif} alt="" className="" />
           {/* <div className="footer-call-number-text">+91-8108012525</div> */}
@@ -101,7 +101,7 @@ function Banner() {
       <div className="z-40 relative mt-[70vh] md:mt-0 flex md:pr-20">
         <div
           id="ee-form-3"
-          className="shadow-xl p-8 bg-white max-w-[450px] rounded-3xl"
+          className="shadow-2xl w-[90%] mx-auto p-2 md:p-8 bg-white max-w-[450px] rounded-3xl"
         ></div>
       </div>
       <div className="h-[100vh] w-full absolute top-0 object-cover">
--- app/boarding-school/admission-open/Components/Hero/components/GoogleReviews.js
+++ app/boarding-school/admission-open/Components/Hero/components/GoogleReviews.js
@@ -1,22 +1,88 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import googleReviewsBackground from "../../../../../../public/LandingPage/googleReviewsBackground.png";
 import Image from "next/image";
+import "swiper/css";
 import { useInView } from "react-intersection-observer";
+import { SwiperSlide, Swiper } from "swiper/react";
+import reviewsDummy from "../../../../../../public/LandingPage/reviewsDummy.png";
+import StarRatings from "react-star-ratings";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { useMobile } from "@/components/globalComponents/IsMobileContext";
 function GoogleReviews() {
   const { ref, inView } = useInView({
     triggerOnce: false,
     threshold: 0.1,
   });
 
+  const { isMobile } = useMobile();
+
+  const features = [
+    {
+      image: reviewsDummy,
+      rating: 5,
+      name: "Apurva Rai",
+      reviewBy: "PROUD PARENT",
+      description:
+        "A school for my kid has to be the best in terms of everything. Being a prestigious institution focusing on overall development, Tula's sounded like the best option!",
+    },
+    {
+      image: reviewsDummy,
+      rating: 4.5,
+      name: "Apurva Rai",
+      reviewBy: "PROUD PARENT",
+      description:
+        "A school for my kid has to be the best in terms of everything. Being a prestigious institution focusing on overall development, Tula's sounded like the best option!",
+    },
+    {
+      image: reviewsDummy,
+      rating: 3.4,
+      name: "Apurva Rai",
+      reviewBy: "PROUD PARENT",
+      description:
+        "A school for my kid has to be the best in terms of everything. Being a prestigious institution focusing on overall development, Tula's sounded like the best option!",
+    },
+    {
+      image: reviewsDummy,
+      rating: 3.5,
+      name: "Apurva Rai",
+      reviewBy: "PROUD PARENT",
+      description:
+        "A school for my kid has to be the best in terms of everything. Being a prestigious institution focusing on overall development, Tula's sounded like the best option!",
+    },
+    {
+      image: reviewsDummy,
+      rating: 4.5,
+      name: "Apurva Rai",
+      reviewBy: "PROUD PARENT",
+      description:
+        "A school for my kid has to be the best in terms of everything. Being a prestigious institution focusing on overall development, Tula's sounded like the best option!",
+    },
+  ];
+
+  const swiperRef = useRef();
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handlePrevClick = () => {
+    if (activeIndex > 0) {
+      swiperRef.current.swiper.slidePrev();
+    }
+  };
+
+  const handleNextClick = () => {
+    if (activeIndex < features.length - 1) {
+      swiperRef.current.swiper.slideNext();
+    }
+  };
+
   return (
-    <div className="flex flex-col md:flex-row md:min-h-[100vh] relative w-full  justify-center">
+    <div className="flex flex-col md:flex-row min-h-screen md:min-h-[75vh] relative w-full">
       <div className="w-full md:w-[42%]  relative flex bg-black flex-col justify-center items-center md:h-full">
         <Image
           src={googleReviewsBackground}
           alt=""
-          className="w-full opacity-50 min-h-[50vh] md:min-h-screen object-cover"
+          className="w-full opacity-50 min-h-[50vh] md:min-h-[75vh] object-cover"
         />
-        <div className="text-white absolute  flex flex-col justify-center items-center px-8">
+        <div className="text-white absolute flex flex-col justify-center items-center px-8">
           <h3
             className="font-[Mirador] text-center"
             style={{ fontSize: "clamp(38px, 4vw, 70px)" }}
@@ -61,7 +127,81 @@ function GoogleReviews() {
           </p3>
         </div>
       </div>
-      <div className="w-full md:w-[58%] min-h-[50vh] md:min-h-screen backgroundGoogle"></div>
+      <div className="w-full md:w-[58%] relative min-h-[50vh] md:min-h-[75vh] backgroundGoogle">
+        <div className="w-full h-fit absolute top-[50%] overflow-hidden -translate-y-1/2">
+          <div
+            className={` ${inView ? "animate-translateRight opacity-100" : "opacity-0"}`}
+          >
+            <Swiper
+              onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
+              ref={swiperRef}
+              slidesPerView={isMobile ? 1.2 : 2.3}
+              centeredSlides={true}
+              spaceBetween={30}
+            >
+              {features.map((feature, index) => (
+                <SwiperSlide key={index}>
+                  <div
+                    className="h-full rounded-xl w-full px-6 py-4 bg-white overflow-hidden mx-auto"
+                    style={{ fontFamily: "TT Chocolates" }}
+                  >
+                    <div className="flex justify-between mb-4 items-center">
+                      <div className="w-fit flex flex-col text-[14px] gap-4 justify-between">
+                        <StarRatings
+                          rating={feature.rating}
+                          starRatedColor="#b90124"
+                          numberOfStars={5}
+                          starDimension="20px"
+                          starSpacing="5px"
+                          name="rating"
+                        />
+                        <h2 className="text-[22px] md:text-[30px] leading-none font-extrabold text-left ">
+                          {feature.name}
+                        </h2>
+                        <h3 className="text-[16px] md:text-[20px] leading-none font-extrabold text-left ">
+                          {feature.reviewBy}
+                        </h3>
+                      </div>
+                      <Image
+                        src={feature.image}
+                        alt=""
+                        className="w-40 aspect-square rounded-full h-full object-cover"
+                      />
+                    </div>
+                    <p className="text-[16px] md:text-[20px]">
+                      {feature.description}
+                    </p>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+          <div className="flex space-x-10 pt-4 justify-end pr-10">
+            <button
+              onClick={handlePrevClick}
+              className={`pointer-events-auto shadow-2xl text-white h-[60px] w-[60px] bg-[#b90124] flex items-center justify-center text-[40px] ${
+                activeIndex === 0
+                  ? "cursor-not-allowed opacity-50"
+                  : "active:scale-90"
+              }`}
+              disabled={activeIndex === 0}
+            >
+              <IoIosArrowBack />
+            </button>
+            <button
+              onClick={handleNextClick}
+              className={`pointer-events-auto shadow-2xl text-white h-[60px] w-[60px] bg-[#b90124] flex items-center justify-center text-[40px] ${
+                activeIndex === features.length - 1
+                  ? "cursor-not-allowed opacity-50"
+                  : "active:scale-90"
+              }`}
+              disabled={activeIndex === features.length - 1}
+            >
+              <IoIosArrowForward />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
