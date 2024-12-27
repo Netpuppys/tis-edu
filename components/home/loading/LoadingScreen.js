56d7179f9aec8feb35a753110ddf8b87c808eaa1
Harsh Kumar rattewal <harshrattewal@gmail.com>
2024-12-27 11:13:39 +0530

    text fix

--- components/home/loading/LoadingScreen.js
+++ components/home/loading/LoadingScreen.js
@@ -22,25 +22,25 @@ function LoadingScreen() {
           >
             with
           </span>{" "}
-          Tula&apos;s{" "}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 268.317 14.075"
-            className={`mainText-scribble ${isActive ? "active" : ""}`}
-          >
-            <path
-              id="mainTextScribble"
-              d="M404.67,1796.978c47.813-3.483,110.6-.1,152.153-3.214s113.059,2.5,113.059,2.5-196.62,2.328-239.976,5.307c85.143,5.178,211.34,0,211.34,0"
-              transform="translate(-403.065 -1791.313)"
-              fill="none"
-              stroke="#c09d59"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="3"
-              className="svg-elem-1"
-            />
-          </svg>
+          Tula&apos;s
         </p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 268.317 14.075"
+          className={`mainText-scribble ${isActive ? "active" : ""}`}
+        >
+          <path
+            id="mainTextScribble"
+            d="M404.67,1796.978c47.813-3.483,110.6-.1,152.153-3.214s113.059,2.5,113.059,2.5-196.62,2.328-239.976,5.307c85.143,5.178,211.34,0,211.34,0"
+            transform="translate(-403.065 -1791.313)"
+            fill="none"
+            stroke="#c09d59"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="3"
+            className="svg-elem-1"
+          />
+        </svg>
       </div>
     </div>
   );
