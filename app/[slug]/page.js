27d9f5d4c37b1cd52f3345c21bf728036bca366a
Harsh Kumar rattewal <harshrattewal@gmail.com>
2024-12-29 20:29:16 +0530

    Wordpress Crm live

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -1,153 +1,19 @@
-// app/[slug]/page.js
-import React from "react";
-import Navbar from "@/components/globalComponents/navbar/Navbar";
-import Footer from "@/components/Footer/Footer";
-import "../globals.css";
-import "../../styles/QuickLinks/Blog.css";
-import "../../styles/globalComponents/Header/header.css";
-import TableOfContent from "@/components/TableOfContent";
-export const revalidate = 60; // optional: ISR for revalidation every 60 seconds
-
-// Generate static params for dynamic routing
-export async function generateStaticParams() {
-  const res = await fetch("https://blog.tis.edu.in/api/v1/post", {
-    timeout: 20000,
-  });
-  const { data } = await res.json();
-
-  if (!Array.isArray(data)) {
-    return [];
-  }
-
-  return data.map((post) => ({
-    slug: post.slug,
-  }));
-}
-
-// Fetch the blog data based on the slug
-async function fetchBlogData(slug) {
-  const res = await fetch(`https://blog.tis.edu.in/api/v1/post/${slug}`);
-
-  if (!res.ok) {
-    throw new Error(`Failed to fetch data for slug: ${slug}`);
-  }
-
-  const { data } = await res.json();
-  return data || null;
-}
-
-export async function generateMetadata({ params }) {
-  const { slug } = params;
-  const blog = await fetchBlogData(slug);
-
-  if (!blog) {
-    return {
-      meta_title: "Blog Not Found",
-      description: "The requested blog could not be found.",
-    };
-  }
-
-  return {
-    title: blog.meta_title,
-    description: blog.meta_description,
-    keywords: blog.meta_keywords,
-    robots: blog.tags,
-    author: blog.author_name,
-  };
-}
-
-// Main page component for the blog post
-export default async function SlugPage({ params }) {
-  const { slug } = params;
-  const blog = await fetchBlogData(slug);
-
-  if (!blog) {
-    return (
-      <div>
-        <h2>Blog not found</h2>
-      </div>
-    );
-  }
-
-  const formatDate = (dateString) => {
-    const options = { year: "numeric", month: "long", day: "numeric" };
-    return new Date(dateString).toLocaleDateString(undefined, options);
-  };
-
-  const formattedDate = formatDate(blog.created_at);
-  const headerImg = blog.banner_img;
-  const cleanContent = (content) => {
-    if (!content) return "";
-    // Replace empty <p></p> with <br /> tag
-    return content.replace(/<p><\/p>/g, "<br /><br />");
-  };
-
-  return (
-    <>
-      <Navbar />
-      <div className="blog-page-container">
-        <div className="header">
-          <div
-            className="heading-outer"
-            style={{
-              backgroundImage: `url(${headerImg})`,
-              backgroundSize: "cover",
-              backgroundRepeat: "no-repeat",
-            }}
-          ></div>
-          <div className="middle">
-            <div className="top">
-              <h2 className="top-heading">
-                {blog.title}
-                <br />
-              </h2>
-            </div>
-          </div>
-        </div>
-        <div
-          className="blog-page-content"
-          style={{ fontFamily: "TT Chocolates" }}
-        >
-          <h6 className="text-[18px] md:text-[25px]">
-            Published on {formattedDate} by {blog.author_name}
-          </h6>
-          <TableOfContent slug={slug} />
-          <div
-            className="content-div-blog"
-            dangerouslySetInnerHTML={{ __html: cleanContent(blog?.content) }}
-          />
-        </div>
-      </div>
-      <Footer />
-    </>
-  );
-}
-
+// // app/[slug]/page.js
 // import React from "react";
-// import Image from "next/image";
 // import Navbar from "@/components/globalComponents/navbar/Navbar";
 // import Footer from "@/components/Footer/Footer";
-// import loader from "../../public/loader.svg"; // Replace with your actual loader image path
 // import "../globals.css";
 // import "../../styles/QuickLinks/Blog.css";
 // import "../../styles/globalComponents/Header/header.css";
+// import TableOfContent from "@/components/TableOfContent";
+// export const revalidate = 60; // optional: ISR for revalidation every 60 seconds
 
-// export const revalidate = 60; // ISR revalidation every 60 seconds
-
+// // Generate static params for dynamic routing
 // export async function generateStaticParams() {
-//   const response1 = await fetch(
-//     "https://admin.tis.edu.in/wp-json/wp/v2/posts?page=1&per_page=100"
-//   );
-//   const response2 = await fetch(
-//     "https://admin.tis.edu.in/wp-json/wp/v2/posts?page=2&per_page=100"
-//   );
-//   const response3 = await fetch(
-//     "https://admin.tis.edu.in/wp-json/wp/v2/posts?page=3&per_page=100"
-//   );
-//   const data1 = await response1.json();
-//   const data2 = await response2.json();
-//   const data3 = await response3.json();
-//   const data = data1.concat(data2, data3);
+//   const res = await fetch("https://blog.tis.edu.in/api/v1/post", {
+//     timeout: 20000,
+//   });
+//   const { data } = await res.json();
 
 //   if (!Array.isArray(data)) {
 //     return [];
@@ -160,16 +26,14 @@ export default async function SlugPage({ params }) {
 
 // // Fetch the blog data based on the slug
 // async function fetchBlogData(slug) {
-//   const res = await fetch(
-//     `https://admin.tis.edu.in/wp-json/wp/v2/posts?slug=${slug}`
-//   );
+//   const res = await fetch(`https://blog.tis.edu.in/api/v1/post/${slug}`);
 
 //   if (!res.ok) {
 //     throw new Error(`Failed to fetch data for slug: ${slug}`);
 //   }
 
-//   const [blogData] = await res.json();
-//   return blogData || null;
+//   const { data } = await res.json();
+//   return data || null;
 // }
 
 // export async function generateMetadata({ params }) {
@@ -178,37 +42,24 @@ export default async function SlugPage({ params }) {
 
 //   if (!blog) {
 //     return {
-//       title: "Blog Not Found",
+//       meta_title: "Blog Not Found",
 //       description: "The requested blog could not be found.",
 //     };
 //   }
 
 //   return {
-//     title: blog?.yoast_head_json?.title,
-//     description: blog?.yoast_head_json?.description,
+//     title: blog.meta_title,
+//     description: blog.meta_description,
+//     keywords: blog.meta_keywords,
+//     robots: blog.tags,
+//     author: blog.author_name,
 //   };
 // }
 
+// // Main page component for the blog post
 // export default async function SlugPage({ params }) {
 //   const { slug } = params;
-//   let blog;
-//   let isLoading = true;
-
-//   try {
-//     blog = await fetchBlogData(slug);
-//     isLoading = false; // Set to false when data is successfully fetched
-//   } catch (error) {
-//     console.error("Error fetching blog data:", error);
-//     isLoading = true; // Remain loading if an error occurs during fetching
-//   }
-
-//   if (isLoading) {
-//     return (
-//       <div className="loader">
-//         <Image src={loader} alt="Loading..." />
-//       </div>
-//     );
-//   }
+//   const blog = await fetchBlogData(slug);
 
 //   if (!blog) {
 //     return (
@@ -223,43 +74,192 @@ export default async function SlugPage({ params }) {
 //     return new Date(dateString).toLocaleDateString(undefined, options);
 //   };
 
-//   const formattedDate = formatDate(blog.date);
-//   const formattedTitle = blog.title.rendered
-//     .replace(/&#8217;/g, "'")
-//     .replace(/&#038;/g, "&");
-//   const headerImg = blog?.yoast_head_json?.og_image?.[0]?.url;
+//   const formattedDate = formatDate(blog.created_at);
+//   const headerImg = blog.banner_img;
+//   const cleanContent = (content) => {
+//     if (!content) return "";
+//     // Replace empty <p></p> with <br /> tag
+//     return content.replace(/<p><\/p>/g, "<br /><br />");
+//   };
 
 //   return (
 //     <>
 //       <Navbar />
-
 //       <div className="blog-page-container">
 //         <div className="header">
 //           <div
 //             className="heading-outer"
 //             style={{
 //               backgroundImage: `url(${headerImg})`,
-//               backgroundSize: "cover", // Adjusted background size to cover for better display
+//               backgroundSize: "cover",
 //               backgroundRepeat: "no-repeat",
 //             }}
 //           ></div>
 //           <div className="middle">
 //             <div className="top">
 //               <h2 className="top-heading">
-//                 {blog?.title?.rendered}
+//                 {blog.title}
 //                 <br />
 //               </h2>
 //             </div>
 //           </div>
 //         </div>
-//         <>
+//         <div
+//           className="blog-page-content"
+//           style={{ fontFamily: "TT Chocolates" }}
+//         >
+//           <h6 className="text-[18px] md:text-[25px]">
+//             Published on {formattedDate} by {blog.author_name}
+//           </h6>
+//           <TableOfContent slug={slug} />
 //           <div
-//             className="blog-page-content"
-//             dangerouslySetInnerHTML={{ __html: blog?.content?.rendered }}
+//             className="content-div-blog"
+//             dangerouslySetInnerHTML={{ __html: cleanContent(blog?.content) }}
 //           />
-//         </>
+//         </div>
 //       </div>
 //       <Footer />
 //     </>
 //   );
 // }
+
+import React from "react";
+import Image from "next/image";
+import Navbar from "@/components/globalComponents/navbar/Navbar";
+import Footer from "@/components/Footer/Footer";
+import loader from "../../public/loader.svg"; // Replace with your actual loader image path
+import "../globals.css";
+import "../../styles/QuickLinks/Blog.css";
+import "../../styles/globalComponents/Header/header.css";
+
+export const revalidate = 60; // ISR revalidation every 60 seconds
+
+export async function generateStaticParams() {
+  const response1 = await fetch(
+    "https://admin.tis.edu.in/wp-json/wp/v2/posts?page=1&per_page=100"
+  );
+  const response2 = await fetch(
+    "https://admin.tis.edu.in/wp-json/wp/v2/posts?page=2&per_page=100"
+  );
+  const response3 = await fetch(
+    "https://admin.tis.edu.in/wp-json/wp/v2/posts?page=3&per_page=100"
+  );
+  const data1 = await response1.json();
+  const data2 = await response2.json();
+  const data3 = await response3.json();
+  const data = data1.concat(data2, data3);
+
+  if (!Array.isArray(data)) {
+    return [];
+  }
+
+  return data.map((post) => ({
+    slug: post.slug,
+  }));
+}
+
+// Fetch the blog data based on the slug
+async function fetchBlogData(slug) {
+  const res = await fetch(
+    `https://admin.tis.edu.in/wp-json/wp/v2/posts?slug=${slug}`
+  );
+
+  if (!res.ok) {
+    throw new Error(`Failed to fetch data for slug: ${slug}`);
+  }
+
+  const [blogData] = await res.json();
+  return blogData || null;
+}
+
+export async function generateMetadata({ params }) {
+  const { slug } = params;
+  const blog = await fetchBlogData(slug);
+
+  if (!blog) {
+    return {
+      title: "Blog Not Found",
+      description: "The requested blog could not be found.",
+    };
+  }
+
+  return {
+    title: blog?.yoast_head_json?.title,
+    description: blog?.yoast_head_json?.description,
+  };
+}
+
+export default async function SlugPage({ params }) {
+  const { slug } = params;
+  let blog;
+  let isLoading = true;
+
+  try {
+    blog = await fetchBlogData(slug);
+    isLoading = false; // Set to false when data is successfully fetched
+  } catch (error) {
+    console.error("Error fetching blog data:", error);
+    isLoading = true; // Remain loading if an error occurs during fetching
+  }
+
+  if (isLoading) {
+    return (
+      <div className="loader">
+        <Image src={loader} alt="Loading..." />
+      </div>
+    );
+  }
+
+  if (!blog) {
+    return (
+      <div>
+        <h2>Blog not found</h2>
+      </div>
+    );
+  }
+
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
+
+  const formattedDate = formatDate(blog.date);
+  const formattedTitle = blog.title.rendered
+    .replace(/&#8217;/g, "'")
+    .replace(/&#038;/g, "&");
+  const headerImg = blog?.yoast_head_json?.og_image?.[0]?.url;
+
+  return (
+    <>
+      <Navbar />
+
+      <div className="blog-page-container">
+        <div className="header">
+          <div
+            className="heading-outer"
+            style={{
+              backgroundImage: `url(${headerImg})`,
+              backgroundSize: "cover", // Adjusted background size to cover for better display
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+          <div className="middle">
+            <div className="top">
+              <h2 className="top-heading">
+                {blog?.title?.rendered}
+                <br />
+              </h2>
+            </div>
+          </div>
+        </div>
+        <>
+          <div
+            className="blog-page-content"
+            dangerouslySetInnerHTML={{ __html: blog?.content?.rendered }}
+          />
+        </>
+      </div>
+      <Footer />
+    </>
+  );
+}
--- components/QuickLinks/Blog.js
+++ components/QuickLinks/Blog.js
@@ -1,120 +1,3 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import Image from "next/image";
-import loader from "../../public/loader.svg";
-import "../../styles/QuickLinks/Blog.css";
-
-const Blog = () => {
-  const [blogs, setBlogs] = useState([]); // Initialize as an empty array
-  const [page, setPage] = useState(1);
-  const [allBlogsLoaded, setAllBlogsLoaded] = useState(false);
-  const [loading, setLoading] = useState(true); // Set loading to true initially
-
-  useEffect(() => {
-    const fetchBlogs = async () => {
-      try {
-        const response = await fetch(
-          `https://blog.tis.edu.in/api/v1/post?page=${page}&per_page=18`
-        );
-        const data = await response.json();
-
-        if (data.success) {
-          const filteredBlogs = data.data.filter(
-            (blog) => blog.category && blog.category.name === "Blogs"
-          );
-
-          if (filteredBlogs.length === 0) {
-            setAllBlogsLoaded(true); // If no blogs are returned, set allBlogsLoaded to true
-          } else {
-            setBlogs((prevBlogs) => {
-              const uniqueBlogs = filteredBlogs.filter(
-                (newBlog) =>
-                  !prevBlogs.some((prevBlog) => prevBlog.slug === newBlog.slug)
-              );
-              return [...prevBlogs, ...uniqueBlogs]; // Append only unique blogs
-            });
-          }
-        } else {
-          console.error("Failed to fetch blogs:", data.message);
-        }
-      } catch (error) {
-        console.error("Error fetching blogs:", error);
-      } finally {
-        setLoading(false); // Set loading to false regardless of success or failure
-      }
-    };
-
-    fetchBlogs();
-  }, [page]);
-
-  const formatDate = (dateString) => {
-    const options = { year: "numeric", month: "long", day: "numeric" };
-    return new Date(dateString).toLocaleDateString(undefined, options);
-  };
-
-  const handleLoadMore = () => {
-    if (!allBlogsLoaded) {
-      setPage((prevPage) => prevPage + 1); // Increment page to fetch more blogs
-    }
-  };
-
-  if (loading && blogs.length === 0) {
-    return (
-      <div className="loader">
-        <Image src={loader} alt="Loading..." />
-      </div>
-    ); // Display loader if still loading and no blogs are fetched
-  }
-
-  return (
-    <>
-      <div className="blog-container">
-        <div className="card-grid">
-          {blogs.map((blog) => {
-            const formattedDate = formatDate(blog.created_at);
-
-            // Check if title and rendered property exist
-            const formattedTitle = blog.title
-              ? blog.title.replace(/&#8217;/g, "'").replace(/&#038;/g, "&")
-              : ""; // Fallback if title is not available
-
-            return (
-              <a href={`/${blog.slug}`} key={blog.slug}>
-                <div>
-                  <div className="card-wrapper">
-                    <Image
-                      width="400"
-                      height="250"
-                      src={blog.banner_img}
-                      className="cardImage"
-                      alt="Blog Image"
-                    />
-                    <div className="card-text">
-                      <p>{formattedDate}</p>
-                      <h5>{formattedTitle}</h5>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            );
-          })}
-        </div>
-        {allBlogsLoaded ? (
-          <button className="load-more" disabled>
-            All Blogs Loaded
-          </button>
-        ) : (
-          <button className="load-more" onClick={handleLoadMore}>
-            Load More
-          </button>
-        )}
-      </div>
-    </>
-  );
-};
-
-export default Blog;
-
 // "use client";
 // import React, { useEffect, useState } from "react";
 // import Image from "next/image";
@@ -131,27 +14,28 @@ export default Blog;
 //     const fetchBlogs = async () => {
 //       try {
 //         const response = await fetch(
-//           `https://admin.tis.edu.in/wp-json/wp/v2/posts?page=${page}&per_page=18`
+//           `https://blog.tis.edu.in/api/v1/post?page=${page}&per_page=18`
 //         );
-
-//         // Check if the response status is OK (200-299)
-//         if (!response.ok) {
-//           throw new Error(`HTTP error! Status: ${response.status}`);
-//         }
-
 //         const data = await response.json();
 
-//         if (Array.isArray(data) && data.length > 0) {
-//           // Assuming that the posts don't have a specific category structure in the response
-//           setBlogs((prevBlogs) => {
-//             const uniqueBlogs = data.filter(
-//               (newBlog) =>
-//                 !prevBlogs.some((prevBlog) => prevBlog.slug === newBlog.slug)
-//             );
-//             return [...prevBlogs, ...uniqueBlogs]; // Append only unique blogs
-//           });
+//         if (data.success) {
+//           const filteredBlogs = data.data.filter(
+//             (blog) => blog.category && blog.category.name === "Blogs"
+//           );
+
+//           if (filteredBlogs.length === 0) {
+//             setAllBlogsLoaded(true); // If no blogs are returned, set allBlogsLoaded to true
+//           } else {
+//             setBlogs((prevBlogs) => {
+//               const uniqueBlogs = filteredBlogs.filter(
+//                 (newBlog) =>
+//                   !prevBlogs.some((prevBlog) => prevBlog.slug === newBlog.slug)
+//               );
+//               return [...prevBlogs, ...uniqueBlogs]; // Append only unique blogs
+//             });
+//           }
 //         } else {
-//           setAllBlogsLoaded(true); // If no blogs are returned, set allBlogsLoaded to true
+//           console.error("Failed to fetch blogs:", data.message);
 //         }
 //       } catch (error) {
 //         console.error("Error fetching blogs:", error);
@@ -184,18 +68,16 @@ export default Blog;
 
 //   return (
 //     <>
-//       {loading && (
-//         <div className="absolute-loader">
-//           <Image src={loader} alt="Loading..." />
-//         </div>
-//       )}
 //       <div className="blog-container">
 //         <div className="card-grid">
 //           {blogs.map((blog) => {
-//             const formattedDate = formatDate(blog.date);
-//             const formattedTitle = blog.title.rendered
-//               .replace(/&#8217;/g, "'")
-//               .replace(/&#038;/g, "&");
+//             const formattedDate = formatDate(blog.created_at);
+
+//             // Check if title and rendered property exist
+//             const formattedTitle = blog.title
+//               ? blog.title.replace(/&#8217;/g, "'").replace(/&#038;/g, "&")
+//               : ""; // Fallback if title is not available
+
 //             return (
 //               <a href={`/${blog.slug}`} key={blog.slug}>
 //                 <div>
@@ -203,7 +85,7 @@ export default Blog;
 //                     <Image
 //                       width="400"
 //                       height="250"
-//                       src={blog?.yoast_head_json?.og_image?.[0]?.url}
+//                       src={blog.banner_img}
 //                       className="cardImage"
 //                       alt="Blog Image"
 //                     />
@@ -232,3 +114,121 @@ export default Blog;
 // };
 
 // export default Blog;
+
+"use client";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import loader from "../../public/loader.svg";
+import "../../styles/QuickLinks/Blog.css";
+
+const Blog = () => {
+  const [blogs, setBlogs] = useState([]); // Initialize as an empty array
+  const [page, setPage] = useState(1);
+  const [allBlogsLoaded, setAllBlogsLoaded] = useState(false);
+  const [loading, setLoading] = useState(true); // Set loading to true initially
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const response = await fetch(
+          `https://admin.tis.edu.in/wp-json/wp/v2/posts?page=${page}&per_page=18`
+        );
+
+        // Check if the response status is OK (200-299)
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        if (Array.isArray(data) && data.length > 0) {
+          // Assuming that the posts don't have a specific category structure in the response
+          setBlogs((prevBlogs) => {
+            const uniqueBlogs = data.filter(
+              (newBlog) =>
+                !prevBlogs.some((prevBlog) => prevBlog.slug === newBlog.slug)
+            );
+            return [...prevBlogs, ...uniqueBlogs]; // Append only unique blogs
+          });
+        } else {
+          setAllBlogsLoaded(true); // If no blogs are returned, set allBlogsLoaded to true
+        }
+      } catch (error) {
+        console.error("Error fetching blogs:", error);
+      } finally {
+        setLoading(false); // Set loading to false regardless of success or failure
+      }
+    };
+
+    fetchBlogs();
+  }, [page]);
+
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
+
+  const handleLoadMore = () => {
+    if (!allBlogsLoaded) {
+      setPage((prevPage) => prevPage + 1); // Increment page to fetch more blogs
+    }
+  };
+
+  if (loading && blogs.length === 0) {
+    return (
+      <div className="loader">
+        <Image src={loader} alt="Loading..." />
+      </div>
+    ); // Display loader if still loading and no blogs are fetched
+  }
+
+  return (
+    <>
+      {loading && (
+        <div className="absolute-loader">
+          <Image src={loader} alt="Loading..." />
+        </div>
+      )}
+      <div className="blog-container">
+        <div className="card-grid">
+          {blogs.map((blog) => {
+            const formattedDate = formatDate(blog.date);
+            const formattedTitle = blog.title.rendered
+              .replace(/&#8217;/g, "'")
+              .replace(/&#038;/g, "&");
+            return (
+              <a href={`/${blog.slug}`} key={blog.slug}>
+                <div>
+                  <div className="card-wrapper">
+                    <Image
+                      width="400"
+                      height="250"
+                      src={blog?.yoast_head_json?.og_image?.[0]?.url}
+                      className="cardImage"
+                      alt="Blog Image"
+                    />
+                    <div className="card-text">
+                      <p>{formattedDate}</p>
+                      <h5>{formattedTitle}</h5>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            );
+          })}
+        </div>
+        {allBlogsLoaded ? (
+          <button className="load-more" disabled>
+            All Blogs Loaded
+          </button>
+        ) : (
+          <button className="load-more" onClick={handleLoadMore}>
+            Load More
+          </button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Blog;
