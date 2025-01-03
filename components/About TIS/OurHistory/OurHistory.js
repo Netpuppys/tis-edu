cacfd3d2c0db39d67418c61a4680cc6fd277392d
Harsh Kumar rattewal <harshrattewal@gmail.com>
2025-01-03 11:50:42 +0530

    sitemap updated

--- components/About TIS/OurHistory/OurHistory.js
+++ components/About TIS/OurHistory/OurHistory.js
@@ -2,23 +2,14 @@
 
 import React from "react";
 import Image from "next/image";
-import Image2004 from "../../../public/ourhistory/2004.jpg";
+import Sunil from "../../../public/ourhistory/Sunil.png";
 import Image2006 from "../../../public/ourhistory/2006.jpg";
 import Image2012 from "../../../public/ourhistory/2012.jpg";
 import Header from "../../globalComponents/Header/Header";
 import "../../../styles/About Tis/ourHistory.css";
 import { useMobile } from "../../globalComponents/IsMobileContext";
 import lineImg from "../../../public/pictures/lineImg.png";
-import headerHistoryMobileImg from "../../../public/Header/HistoryHeaderMobile.jpg";
-import headerHistoryImg from "../../../public/Header/HistoryHeader.jpg";
-const CarouselCard = ({ title, description }) => {
-  return (
-    <div className="carousel-item">
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
-};
+import headerHistoryImg from "../../../public/Header/HistoryHeader.png";
 
 export default function OurHistory() {
   const subTitle =
@@ -30,10 +21,55 @@ export default function OurHistory() {
       <Header
         title={"Our History"}
         subtitle={subTitle}
-        headerImg={isMobile ? headerHistoryMobileImg : headerHistoryImg}
+        headerImg={headerHistoryImg}
       />
+      <div className="py-8 w-[70%] mx-auto md:py-20 flex flex-col justify-center items-center">
+        <h3
+          style={{
+            fontFamily: "TT Chocolates",
+          }}
+          className="text-[#B90124] w-fit leading-none text-[clamp(20px,2.5vw,50px)]"
+        >
+          Flip The Pages of Our Inception Story
+          <Image src={lineImg} alt="" className="ml-auto" />
+        </h3>
+        <div className={`flex gap-20 py-8 md:py-20`}>
+          <div className="relative w-[50%] mx-auto group overflow-hidden">
+            <div className="w-full h-full ">
+              <Image
+                src={Sunil}
+                alt=""
+                className="w-full h-full object-cover transition-all duration-300 group-hover:scale-110 group-hover:opacity-80"
+              />
+              {/* Animated border */}
+              {!isMobile && (
+                <>
+                  <div className="absolute block bottom-10 left-10 h-full w-[1px] bg-white transition-all duration-500 ease-in-out group-hover:left-0 group-hover:bottom-0"></div>
+                  <div className="absolute block bottom-10 left-10 h-[1px] w-full bg-white transition-all duration-500 ease-in-out group-hover:left-0 group-hover:bottom-0"></div>
+                </>
+              )}
+            </div>
+          </div>
+          <div className="flex w-full flex-col items-start justify-center text-black">
+            <h3
+              className="w-full text-[#212121] font-normal text-[clamp(16px,1.3vw,40px)]"
+              style={{
+                fontFamily: "TT Chocolates",
+              }}
+            >
+              In 2004, <span className="text-[#b90124]">Sunil Kumar Jain</span>{" "}
+              (Founder, Chairman) stepped into the world of education, driven by
+              a belief that every student deserved more than just a
+              classroom—they deserved a stage to grow, dream, and thrive. His
+              journey began with the Rishabh Trust and was followed by the
+              foundation of Tula’s Institute, a college that quickly became
+              known for its excellence in engineering and management education. 
+            </h3>
+          </div>
+        </div>
+      </div>
 
-      <div className="our-history-desc">
+      {/* <div className="our-history-desc">
         <p className="our-history-desc-p">
           Tulas International School (TIS) was founded with the goal of
           providing students with a world-class education while also encouraging
@@ -44,7 +80,7 @@ export default function OurHistory() {
           foundation stone for TIS in 2012.
         </p>
 
-        <div className="carousel-container">
+         <div className="carousel-container">
           <CarouselCard
             title="#1"
             description=<>
@@ -77,12 +113,11 @@ export default function OurHistory() {
               <br /> and clubs
             </>
           />
-          {/* Add more items as needed */}
-        </div>
-      </div>
-      <div className="our-history-main-content">
+        </div> 
+      </div>*/}
+      {/* <div className="our-history-main-content">
         <div className="our-history-main">
-          <Image src={Image2004} alt="" />
+          <Image src={Sunil} alt="" />
           <div className="content-history">
             <h3>2004</h3>
             <h5>
@@ -150,7 +185,7 @@ export default function OurHistory() {
           </h2>
           <br />
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
