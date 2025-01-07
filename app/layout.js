d6b611353d39cb6158c8521eb52effe96a257969
Harsh Kumar rattewal <harshrattewal@gmail.com>
2025-01-07 16:36:48 +0530

    font changes

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Caveat&family=Bree+Serif&display=swap"
+          rel="stylesheet"
+        ></link>
         {/* Google Tag Manager */}
         <Script
           strategy="afterInteractive"
