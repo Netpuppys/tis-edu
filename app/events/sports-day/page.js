a44cd24172cb428174e6d9af422d61a1c49b61c3
Harsh Kumar rattewal <harshrattewal@gmail.com>
2025-01-09 17:06:55 +0530

    changes

--- app/events/sports-day/page.js
+++ app/events/sports-day/page.js
@@ -180,7 +180,7 @@ export default function EventConfluencePage() {
                   className="w-full h-full object-cover absolute"
                 />
                 <div className="absolute z-10 bg-[#b90124] bg-opacity-70 w-full h-full transition-all ease-in duration-700 translate-y-full group-hover:translate-y-0"></div>
-                <div className="absolute z-20 transition-all ease-in duration-300">
+                <div className="absolute z-20">
                   <h3 className="font-[TTChocolatesBold] w-fit mx-auto text-[clamp(20px,1.8vw,50px)] text-white text-center">
                     {cards.title}
                   </h3>
@@ -248,7 +248,7 @@ export default function EventConfluencePage() {
                         className="w-full h-full object-cover absolute"
                       />
                       <div className="absolute z-10 bg-[#b90124] bg-opacity-70 w-full h-full transition-all ease-in duration-700 translate-y-full group-hover:translate-y-0"></div>
-                      <div className="absolute z-20 transition-all ease-in duration-300">
+                      <div className="absolute z-20">
                         <h3 className="font-[TTChocolatesBold] w-fit mx-auto text-[clamp(20px,1.3vw,50px)] text-white text-center">
                           {cards.title}
                         </h3>
