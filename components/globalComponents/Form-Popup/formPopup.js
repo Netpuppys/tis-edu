effec1faad3ba759dd3353aa54ff11449d16f0ad
Harsh Kumar rattewal <harshrattewal@gmail.com>
2025-03-10 16:15:55 +0530

    otp api changed

--- components/globalComponents/Form-Popup/formPopup.js
+++ components/globalComponents/Form-Popup/formPopup.js
@@ -137,7 +137,7 @@ function FormEnquire({ formPopup }) {
   const sendOtp = async () => {
     setLoading(true);
     axios
-      .post("https://otp.tulas.edu.in/send-otp", {
+      .post("https://otpapi.tulas.edu.in/send-otp", {
         mobileNumber: formData.MobileNumber, // Replace with dynamic mobile number
         message:
           "Hello, ##OTP## is your One Time Password(OTP) forTulas This OTP is valid till 3mins Tulas.", // Replace with your SMS template
@@ -156,7 +156,7 @@ function FormEnquire({ formPopup }) {
     if (isOtpSent) {
       setLoading(true);
       axios
-        .post("https://otp.tulas.edu.in/verify-otp", {
+        .post("https://otpapi.tulas.edu.in/verify-otp", {
           mobileNumber: formData.MobileNumber, // Replace with dynamic mobile number
           otp: otp,
         })
@@ -178,7 +178,7 @@ function FormEnquire({ formPopup }) {
   const resendOtp = async () => {
     setLoading(true);
     axios
-      .post("https://otp.tulas.edu.in/retry-otp", {
+      .post("https://otpapi.tulas.edu.in/retry-otp", {
         mobileNumber: formData.MobileNumber, // Replace with dynamic mobile number
       })
       .then((response) => {
