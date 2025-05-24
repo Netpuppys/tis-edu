1183c2aa2ef8af6a1e8449e14866c899025d6c61
Harsh Kumar rattewal <harshrattewal@gmail.com>
2025-05-24 11:31:28 +0530

    otp changed

--- components/home/components/form/form.js
+++ components/home/components/form/form.js
@@ -131,7 +131,7 @@ function Form() {
   const sendOtp = async () => {
     setLoading(true);
     axios
-      .post("https://otpapi.tulas.edu.in/send-otp", {
+      .post("https://otp.tulas.edu.in/send-otp", {
         mobileNumber: formData.MobileNumber, // Replace with dynamic mobile number
         message:
           "Hello, ##OTP## is your One Time Password(OTP) forTulas This OTP is valid till 3mins Tulas.", // Replace with your SMS template
@@ -151,7 +151,7 @@ function Form() {
     if (isOtpSent) {
       setLoading(true);
       axios
-        .post("https://otpapi.tulas.edu.in/verify-otp", {
+        .post("https://otp.tulas.edu.in/verify-otp", {
           mobileNumber: formData.MobileNumber, // Replace with dynamic mobile number
           otp: otp,
           authkey: "428845Ai6NJKsbqd66c98932P1",
@@ -174,7 +174,7 @@ function Form() {
   const resendOtp = async () => {
     setLoading(true);
     axios
-      .post("https://otpapi.tulas.edu.in/retry-otp", {
+      .post("https://otp.tulas.edu.in/retry-otp", {
         mobileNumber: formData.MobileNumber, // Replace with dynamic mobile number
         authkey: "428845Ai6NJKsbqd66c98932P1",
       })
