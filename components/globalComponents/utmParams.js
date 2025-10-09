7acf18efb90f3242261ebfafe3587e7f28a404b7
netpuppys <digital@netpuppys.com>
2025-10-09 17:25:50 +0530

    chaanges

--- components/globalComponents/utmParams.js
+++ components/globalComponents/utmParams.js
@@ -1,22 +1,24 @@
 "use client";
 import React, { createContext, useState, useEffect } from "react";
 
-// Create the context
 export const UtmContext = createContext();
 
-// Create the provider component
 export const UtmProvider = ({ children }) => {
   const [utmParams, setUtmParams] = useState("");
 
   useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const utmParamsString = params.toString();
-    if (utmParamsString) {
-      setUtmParams(`?${utmParamsString}`);
+    if (typeof window !== "undefined") { // <- ensures SSR safety
+      const params = new URLSearchParams(window.location.search);
+      const utmParamsString = params.toString();
+      if (utmParamsString) {
+        setUtmParams(`?${utmParamsString}`);
+      }
     }
   }, []);
 
   return (
-    <UtmContext.Provider value={{ utmParams }}>{children}</UtmContext.Provider>
+    <UtmContext.Provider value={{ utmParams }}>
+      {children}
+    </UtmContext.Provider>
   );
 };
