b634e1410532c29dbca0b9d36eb2f4c1108f0458
netpuppys <digital@netpuppys.com>
2025-10-13 14:05:25 +0530

    testing

--- components/Pages/Home/Home.js
+++ components/Pages/Home/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
 import HomeLayout from "../../home/HomeLayout";
 import SeoContent from "./SeoContent";
-import BackupHomeLayout from "@/components/home/BackupHomeLayout";
+// import BackupHomeLayout from "@/components/home/BackupHomeLayout";
+import dynamic from "next/dynamic";
+
+// Dynamically import client-only component
+const BackupHomeLayout = dynamic(
+  () => import("@/components/home/BackupHomeLayout"),
+  { ssr: false } // do NOT server-render this
+);
+
 
 function HomePage() {
   return (
