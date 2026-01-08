eb6f563b9fe1f411a89c4b0c40437bc7a8657736
netpuppys <digital@netpuppys.com>
2026-01-08 14:47:58 +0530

    gtm

--- app/layout.js
+++ app/layout.js
@@ -36,7 +36,7 @@ export default function RootLayout({ children }) {
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KM9L7F6F"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KR9HW9RM"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
@@ -49,20 +49,10 @@ export default function RootLayout({ children }) {
         </ProvidersWrapper>
 
         {/* 🧠 Scripts for analytics (load AFTER page renders) */}
-
         {/* Google Tag Manager */}
         <Script
-          id="gtm-script"
           strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-KM9L7F6F');
-            `,
-          }}
+          src="https://www.googletagmanager.com/gtm.js?id=GTM-KR9HW9RM"
         />
 
         {/* Google Analytics */}
